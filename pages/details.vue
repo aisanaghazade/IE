9d5f9b6c8918c09adacc5cfe2e6d077886b75c68
aisanaghazade <aaghazaden@gmail.com>
2018-07-16 01:32:04 +0430

    Revert "buying page: doing"
    This reverts commit 69e67e52e3c0be48c4da32c8c4a8b22d9519d0f3.

--- pages/details.vue
+++ pages/details.vue
@@ -4,20 +4,6 @@
         <multipictureslider></multipictureslider>
         <Product></Product>
         <productinfo></productinfo>
-        <div id= "tabs">
-            <b-tabs id = "tabs1">
-                <b-tab title="مشخصات محصولات" active id = "tab1"> 
-                    <div> 
-                        <b-table striped hover :items="items" id="table"></b-table>
-                    </div>
-                </b-tab>
-                <b-tab title="نظرات کاربران" id = "tab2">
-                    <div>
-
-                    </div>
-                </b-tab>
-            </b-tabs>
-        </div>
         <multiItemSlider></multiItemSlider>
         <footer1></footer1>
     </div>
@@ -30,28 +16,9 @@ import footer1 from "@/components/footer1";
 import header1 from "@/components/header1";
 import Product from "@/components/Product";
 import productinfo from "@/components/productinfo";
-
-const items = [
-  { مشخصه: 'a', مقدارمشخصه: 'salam masalan' },
-  { مشخصه: 'b', مقدارمشخصه: 'salam masalan' },
-  { مشخصه: 'ab', مقدارمشخصه: 'salam masalan' },
-  { مشخصه: 'abc', مقدارمشخصه: 'salam masalan' },
-  { مشخصه: 'abcd', مقدارمشخصه: 'salam masalan' },
-  { مشخصه: 'b', مقدارمشخصه: 'salam masalan' },
-  { مشخصه: 'ab', مقدارمشخصه: 'salam masalan' },
-  { مشخصه: 'abc', مقدارمشخصه: 'salam masalan' },
-  { مشخصه: 'abcd', مقدارمشخصه: 'salam masalan' },
-  { مشخصه: 'abcde', مقدارمشخصه: 'salam masalan' }
-]
-
 export default {
     components:{
         multiItemSlider, multipictureslider, header1, footer1, Product, productinfo
-    },
-    data () {
-    return {
-      items: items
-    }
     }
 }
 </script>
@@ -61,38 +28,6 @@ export default {
 #details{
     width: 1200px;
     position: absolute;
-}
-
-#tabs{
-    position: absolute;
-    top: 850px;
-    width: 900px;
-    right: 100px;
-}
-
-#nazarat{
-    position: absolute;
-    top: 1000px;
-    width: 900px;
-    border-style: solid;
-
-}
-
-#tab1{
-    position: relative;
-    top: 0px;
-    width: 1000px;
-    right: 0px;
-    height: 300px;
-    overflow-y: scroll;
-    overflow-x: scroll;
-}
-
-#table{
-    direction: rtl;
-    position: absolute;
-    right: 100px;
-    text-align: center;
 }
 
 </style>
--- pages/test.vue
+++ pages/test.vue
@@ -1,182 +1,49 @@
 <template>
-    <tr v-for="(item) in rows" :key='item'>
-                <td class= "c11">
-                    <span class="img"><img class="image" src=""></span>
-                    <span class="info">
-                        <p class="text1">{{item.brandname}}</p>
-                        <p class = "text2">{{item.name1}} {{item.name2}} {{item.name3}}</p>
-                        <p class="text2">رنگ:{{item.color}}</p>
-                        <p class="text2">سایز:{{item.size}}</p>
-                        <p class="text2">کد کالا:{{item.code}}</p>
-                    </span>
-                </td>
-                <td class = "c21">
-                    <span><button id="plus" @click = "plus(item)">+</button></span>
-                    <span id="count"> {{item.count}} </span>
-                    <span><button id="plus" @click = "minus(item)">-</button></span>
-                    
-                </td>
-                <td class = "c31"></td>
-                <td class = "c41"></td>
-            </tr>
+  <md-card>
+    <md-card-actions>
+      <div class="md-subhead">
+        <span>Vertical Slider</span>
+        <span>（</span>
+        <span>垂直切换</span>
+        <span>）</span>
+      </div>
+      <md-button class="md-icon-button"
+                 target="_blank"
+                 href="https://github.com/surmon-china/vue-awesome-swiper/blob/master/examples/09-vertical.vue">
+        <md-icon>code</md-icon>
+      </md-button>
+    </md-card-actions>
+    <md-card-media>
+      <!-- swiper -->
+      <swiper :options="swiperOption">
+        <swiper-slide>Slide 1</swiper-slide>
+        <swiper-slide>Slide 2</swiper-slide>
+        <swiper-slide>Slide 3</swiper-slide>
+        <swiper-slide>Slide 4</swiper-slide>
+        <swiper-slide>Slide 5</swiper-slide>
+        <swiper-slide>Slide 6</swiper-slide>
+        <swiper-slide>Slide 7</swiper-slide>
+        <swiper-slide>Slide 8</swiper-slide>
+        <swiper-slide>Slide 9</swiper-slide>
+        <swiper-slide>Slide 10</swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+    </md-card-media>
+  </md-card>
 </template>
 
 <script>
   export default {
-    
+    data() {
+      return {
+        swiperOption: {
+          direction: 'vertical',
+          pagination: {
+            el: '.swiper-pagination',
+            clickable: true
+          }
+        }
+      }
+    }
   }
 </script>
-
-<table>
-            <tr>
-                <td class= "c11">
-                    <span class="img"><img class="image" src=""></span>
-                    <span class="info">
-                        <p class="text1">{{brandname}}</p>
-                        <p class = "text2">{{name1}} {{name2}} {{name3}}</p>
-                        <p class="text2">رنگ:{{color}}</p>
-                        <p class="text2">سایز:{{size}}</p>
-                        <p class="text2">کد کالا:{{code}}</p>
-                    </span>
-                </td>
-                <td class = "c21">
-                    <span><button id="plus" @click = "plus()">+</button></span>
-                    <span id="count"> {{count}} </span>
-                    <span><button id="plus" @click = "minus()">-</button></span>
-                    
-                </td>
-                <td class = "c31"></td>
-                <td class = "c41"></td>
-            </tr>
-        </table>
-
-
-
-
-
-        <template>
-    <div id = "table">
-        <table id = 'table1'>
-            <tr>
-                <th class = 'c1' id = 'r1c1'>مشخصات محصول</th>
-                <th class = 'c2' id = 'r1c2'>تعداد</th>
-                <th class = 'c3' id = 'r1c3'>قیمت</th>
-                <th class = 'c4' id = 'r1c4'></th>
-            </tr>
-        </table>
-        <div id = "rows">
-            <row v-for="r in rows" :key='r' ></row>
-        </div>
-    </div>
-</template>
-
-<script>
-import row from '@/components/row';
-export default {
-    components:{
-        row
-    },
-    data(){
-        return{
-            a:1,
-            rows:[
-                {item: 1, brandname: 'nike', name1: 'name', name2: 'name', name3: 'name', color: 'black', size: 36, code:'123456', scr:"", count:1},
-                {item: 2, brandname: 'nike', name1: 'name', name2: 'name', name3: 'name', color: 'black', size: 36, code:'123456', scr:"", count:1}
-            ]
-        }
-    }
-}
-</script>
-
-<style scoped>
-@import "../pages/CSSFiles/general.css";
-#rows{
-    position:absolute
-}
-#table{
-    position: absolute;
-    top: 400px;
-    right: 150px;
-    width: 903px;
-    float: right;
-    /* border-style: solid; */
-    direction: rtl;
-    height: 450px;
-    overflow-y: scroll;
-}
-
-#table1{
-    position: relative;
-    top: 0px;
-    right: 0px;
-    width: 900px;
-    border-style: solid;
-    border-width: 0px;
-    float: right;
-    border-style: solid;
-    direction: rtl;
-}
-
-.c1{
-    width: 330px;
-    direction: rtl;
-    text-align: center;
-    border-color: rgb(190, 190, 190);
-    border-right-style: solid;
-    border-left-style: none;
-}
-
-.c2{
-    width: 210px;
-    direction: rtl;
-    text-align: center;
-    border-color: rgb(190, 190, 190);
-    border-right-style: solid;
-    border-left-style: none;
-}
-
-.c3{
-    width: 330px;
-    direction: rtl;
-    text-align: center;
-    border-color: rgb(190, 190, 190);
-    border-right-style: solid;
-    border-left-style: none;
-}
-
-.c4{
-    width: 40px;
-    direction: rtl;
-    text-align: center;
-    border-color: rgb(190, 190, 190);
-    border-right-style: solid;
-    border-left-style: solid;
-    border-bottom-style: solid;
-}
-
-#r1c1{
-    background-color: rgba(241,245,248);
-    border-top-style: none;
-    border-bottom-style: none;
-    height: 35px;
-}
-#r1c2{
-    background-color: rgba(241,245,248);
-    border-top-style: none;
-    border-bottom-style: none;
-}
-#r1c3{
-    background-color: rgba(241,245,248);
-    border-top-style: none;
-    border-bottom-style: none;
-}
-#r1c4{
-    background-color: rgba(241,245,248);
-    border-top-style: none;
-    border-bottom-style: none;
-}
-
-
-</style>
-
-
