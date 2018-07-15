69e67e52e3c0be48c4da32c8c4a8b22d9519d0f3
aisanaghazade <aaghazaden@gmail.com>
2018-07-16 01:31:18 +0430

    buying page: doing

---
+++ pages/buying.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id = 'buying'>
+        <cart v-if="a == 0"></cart>
+        <sending v-if="a == 1"></sending>
+        <payment v-if="a == 2"></payment>
+    </div>
+</template>
+
+<script>
+import cart from '@/components/cart'
+import sending from '@/components/sending';
+import payment from '@/components/payment';
+export default {
+    components:{
+        cart,sending,payment
+    },
+    data(){
+        return{
+            a:0
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+@import "./CSSFiles/general.css";
+</style>
+
+
